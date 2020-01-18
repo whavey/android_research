17e295038a059face36b280ff1cb28ed4459e06e
Wayne <whavey@mail.vast.uccs.edu>
2020-01-18 01:52:48 -0700

    more meta

--- tools/ComparisonAnalysis.py
+++ tools/ComparisonAnalysis.py
@@ -82,6 +82,7 @@ def getStats(cat, catlist):
             print("no results")
 
         analysis_dict[cat]["meta"] = {
+                "num_apps": len(catlist),
                 "aggregate": total, 
                 "agg_avg": total/count, 
                 "avg": total_avg/count, 
@@ -101,16 +102,32 @@ app_count = 0
 analysis_dict = {}
 
 master_dict = {}
+highest_avg_score = (0, None)
+highest_agg_score = (0, None)
+highest_avg_score_nz = (0, None)
+
 for cat in os.listdir("full_results"):
     resfiles = [f for f in os.listdir(os.path.join("full_results", cat)) if "results-" in f]
     master_dict[cat] = resfiles
     analysis_dict[cat] = {}
     getStats(cat, master_dict[cat])
 
+    cat_avg = analysis_dict[cat]["meta"]["avg"]
+    if  cat_avg > highest_avg_score[0]:
+        highest_avg_score = (cat_avg, cat)
+
+    cat_avg_nz = analysis_dict[cat]["meta"]["avg_nz"]
+    if  cat_avg_nz > highest_avg_score_nz[0]:
+        highest_avg_score_nz = (cat_avg_nz, cat)
+
+    cat_agg = analysis_dict[cat]["meta"]["aggregate"]
+    if  cat_agg > highest_agg_score[0]:
+        highest_agg_score = (cat_agg, cat)
+
     print(f"\nWithin Category Analysis:")
     most_diff = (0, None)
     most_diff_nz = (0, None)
-    for f in list(analysis_dict[cat].keys())[1:]:
+    for f in list(analysis_dict[cat].keys())[:-1]:
         diff = analysis_dict[cat][f]["avg"] - analysis_dict[cat]["meta"]["avg"]
         if abs(diff) > most_diff[0]:
             most_diff = (diff, f)
@@ -122,8 +139,24 @@ for cat in os.listdir("full_results"):
         print(f"\t{f} ({analysis_dict[cat][f]['avg']}):\tdifference from avg:", diff)
         print(f"\t{f} ({analysis_dict[cat][f]['avg']}):\tdifference from avg (NO ZEROS):", diff_nz)
 
+        analysis_dict[cat][f].update({"diff_from_avg": diff, "diff_from_avg_nz": diff_nz})
+
+    analysis_dict[cat].update({"most_diff_from_avg": {"app": most_diff[1], "diff": most_diff[0]}, "most_diff_from_avg_nz": {"app": most_diff_nz[1], "diff": most_diff_nz[0]} })
     print(f"\n\t{most_diff[1]} was the APK that differed from category avg the most by: {most_diff[0]}.")
     print(f"\t{most_diff_nz[1]} was the APK that differed from category avg the most (NO ZEROS) by: {most_diff_nz[0]}.")
+
+analysis_dict["meta"] = {
+        "total_apps": app_count,
+        "overall_aggregate": master_total,
+        "overall_aggregate_avg": master_total/master_count,
+        "overall_aggregate_avg_nz": master_total/master_count_no_zeros,
+        "overall_sensitivity_avg": master_avg/master_count,
+        "overall_sensitivity_avg_nz": master_avg_no_zeros/master_count_no_zeros,
+        "total_10_sensitivity_ratings": master_number_of_tens,
+        "highest_category_avg": {"category": highest_avg_score[1], "average": highest_avg_score[0]},
+        "highest_category_avg_nz": {"category": highest_avg_score_nz[1], "average": highest_avg_score_nz[0]},
+        "highest_category_agg": {"category": highest_agg_score[1], "average": highest_agg_score[0]}
+        }
 
 print("\n\nOverall Analysis")
 print("Total APPs:", app_count)
@@ -133,3 +166,8 @@ print("Overall aggregate score average (NO ZEROS):", master_total/master_count_n
 print("Overall privacy sensitivity average:", master_avg/master_count)
 print("Overall privacy sensitivity average (NO ZEROS):", master_avg_no_zeros/master_count_no_zeros)
 print("Total number of 10 sensitivity ratings:", master_number_of_tens)
+print("highest category average:", highest_avg_score[1], "with:",  highest_avg_score[0])
+print("highest category average (NO ZEROS):", highest_avg_score_nz[1], "with:",  highest_avg_score_nz[0])
+print("highest category aggregate:",  highest_agg_score[1], "with:", highest_agg_score[0])
+
+print(json.dumps(analysis_dict), file=open("post_analysis.json", "w"))
